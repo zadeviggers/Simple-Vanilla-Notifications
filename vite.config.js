26f91422b1ba935e8a9f92b9c8e123d25c8a9db4
Zade Viggers <zade@viggers.net>
2022-08-06 23:05:51 +1200

    Add demo build

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
 
+console.log(process.env.BUILD_DEMO);
+const buildLibrary = process.env.BUILD_DEMO ? false : true;
+
 export default defineConfig({
 	plugins: [
-		dts({
-			entryRoot: "lib",
-		}),
+		buildLibrary &&
+			dts({
+				entryRoot: "lib",
+			}),
 	],
-	build: {
+	build: buildLibrary && {
 		lib: {
 			entry: resolve(__dirname, "lib/index.ts"),
 			name: "Simple Vanilla Notifications",
