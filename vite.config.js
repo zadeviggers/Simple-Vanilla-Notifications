2f9976f15534707917e13657c6ecdf3173d25d8c
Zade Viggers <zade@viggers.net>
2022-08-06 22:46:32 +1200

    Fix type declarations output

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-
+import dts from "vite-plugin-dts";
 export default defineConfig({
+	plugins: [
+		dts({
+			entryRoot: "lib",
+		}),
+	],
 	build: {
 		lib: {
 			entry: resolve(__dirname, "lib/index.ts"),
