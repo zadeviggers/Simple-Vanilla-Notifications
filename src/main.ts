ef29cde9dbd0649560c79155957f4b0a1487bf54
Zade Viggers <zade@viggers.net>
2022-11-14 14:18:41 +1300

    Add animate disable thing and better notif generation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import "../lib/defaults.css";
 import "./styles.css";
-import { createNotificationManager, getNextNotificationID } from "../lib";
+import {
+	createNotificationManager,
+	getNextNotificationID,
+	NotificationContents,
+} from "../lib";
 
 const { createNotification, activeNotifications, dismissAllNotifications } =
 	createNotificationManager();
@@ -14,17 +18,63 @@ const dismissButtonCheckbox: HTMLInputElement = document.getElementById(
 	"dismiss-button"
 ) as HTMLInputElement;
 
+const animateCheckbox: HTMLInputElement = document.getElementById(
+	"animate"
+) as HTMLInputElement;
+
+function getRandomMessage(): NotificationContents {
+	const num = Math.floor(Math.random() * 6);
+	switch (num) {
+		case 0: {
+			const id = getNextNotificationID(activeNotifications);
+			return `Hello there! I am number #${id}`;
+		}
+		case 1:
+			return "Looooooooooooooooooooooooong";
+		case 2:
+			return "🫘 Emoji! 💯";
+		case 3: {
+			const el1 = document.createElement("span");
+			const el2 = document.createElement("span");
+
+			el1.appendChild(document.createTextNode("Ooh, look: "));
+
+			el2.setAttribute(
+				"style",
+				"font-style: italic; background-color: red; color: black; background-color: AccentColor; color: AccentColorText;"
+			);
+			el2.innerText = "colour";
+
+			el1.appendChild(el2);
+
+			el1.appendChild(document.createTextNode("!"));
+
+			return el1;
+		}
+		case 4:
+			return "6*9+6+9=8.30662386292²";
+		case 5: {
+			const a = document.createElement("a");
+			a.innerText = "| ||\n|| |_";
+			a.setAttribute("href", "https://cad-comic.com/comic/loss/");
+			a.setAttribute("target", "_blank");
+			a.setAttribute("rel", "noopener noreferrer");
+			a.style.color = "currentColor";
+			a.style.textDecoration = "none";
+			return a;
+		}
+		default:
+			return "Beans";
+	}
+}
+
 (
 	document.getElementById("create-notification-button") as HTMLButtonElement
 ).addEventListener("click", () => {
-	const id = getNextNotificationID(activeNotifications);
-	createNotification(`Hello there! I am number #${id}`, {
+	createNotification(getRandomMessage(), {
 		autoDismissTimeout: autoDismissCheckbox.checked ? 3200 : 0,
 		dismissible: dismissButtonCheckbox.checked,
-	});
-	createNotification("Looooooooooooooooooooooooong", {
-		autoDismissTimeout: autoDismissCheckbox.checked ? 3200 : 0,
-		dismissible: dismissButtonCheckbox.checked,
+		animated: animateCheckbox.checked,
 	});
 });
 
